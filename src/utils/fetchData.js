f5385ac0b315ea1dbf7a725f81805d73ec69ce2c
siddartha-004 <siddarthareddy.d02@gmail.com>
2024-03-23 10:42:05 +0530

    commit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '869c4b2184msheb23ccc7c11105dp15e1a7jsn37e576bb5436',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   },
 };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY_VIDS,
+    'X-RapidAPI-Key':'869c4b2184msheb23ccc7c11105dp15e1a7jsn37e576bb5436',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   },
 };
